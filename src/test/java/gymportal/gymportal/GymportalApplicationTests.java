7679096f740ac49d3e846ef9b8b10bd7977b4572
Djordje Jeremic <djordjejeremic13@gmail.com>
2020-01-19 19:42:46 +0100

    Spring-boot application initial setup.

---
+++ src/test/java/gymportal/gymportal/GymportalApplicationTests.java
@@ -0,0 +1,13 @@
+package gymportal.gymportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
